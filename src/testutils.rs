85de5e9bdad3a61e4dad9f3302cec12535ae4274
Phil Dawes <racerphil@phildawes.net>
2014-05-12 21:14:05 +0100

    oops - add testutils.rs

---
+++ src/testutils.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+pub fn rejustify(src: &str) -> StrBuf {
+    let s = src.slice_from(1); // remove the newline
+    let mut sb = StrBuf::new();
+    for l in s.lines() {
+        let tabless = l.slice_from(4);
+        sb.push_str(tabless); 
+        if tabless.len() != 0 { 
+            sb.push_str("\n");
+        }
+    }
+    let newlen = sb.len()-1; // remove the trailing newline
+    sb.truncate(newlen);
+    return sb;
+}
+
+#[cfg(test)]
+pub fn slice<'a>(src: &'a str, (begin, end): (uint, uint)) -> &'a str{
+    return src.slice(begin, end);
+}
