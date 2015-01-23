798dfc962bfe4a2b4db4cd783706095e976aa406
Johann Tuffe <tafia973@gmail.com>
2015-01-23 13:06:21 +0800

    Add bench_iter_stmts

--- src/racer/bench.rs
+++ src/racer/bench.rs
@@ -2,22 +2,35 @@ extern crate test;
 
 use std::io::fs::PathExtensions;
 use std::os::getenv;
-use racer::codecleaner::code_chunks;
 use std::io::File;
 use self::test::Bencher;
+use racer::codecleaner::code_chunks;
+use racer::codeiter::iter_stmts;
+
+fn get_rust_file_str(path: &[&str]) -> String {
+
+    let mut src_path = match getenv("RUST_SRC_PATH") {
+        Some(env) => { Path::new(&env[]) },
+        None => panic!("Cannot find $RUST_SRC_PATH")
+    };
+    for &s in path.iter() { src_path.push(s); }
+
+    File::open(&src_path).read_to_string().unwrap()
+}
 
 #[bench]
 fn bench_code_chunks(b: &mut Bencher) {
-    
-    let mut src_path = Path::new(&getenv("RUST_SRC_PATH").unwrap()[]);
-    src_path.push("libcollections");
-    src_path.push("bit.rs");
-    
-    let src = &File::open(&src_path).read_to_string().unwrap()[];
-    
+    let src = &get_rust_file_str(&["libcollections", "bit.rs"])[];
     b.iter(|| {
         let chunks = code_chunks(src).collect::<Vec<_>>();
     });
 }
 
+#[bench]
+fn bench_iter_stmts(b: &mut Bencher) {
+    let src = &get_rust_file_str(&["libcollections", "bit.rs"])[];
+    b.iter(|| {
+        let chunks = iter_stmts(src).collect::<Vec<_>>();
+    });
+}
 
