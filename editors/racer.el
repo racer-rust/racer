2a92d924f0d2c9ba7931c899ace9ae56f050f472
ane <ane@iki.fi>
2015-04-19 22:48:31 +0300

    Rework the completion backend to behave more like standard company backend.
    Most of the changes are implemented such that the backend behaves more like other backends.
    This means, for example, that company-mode isn't enabled automatically when the package is loaded,
    the backend must be manually inserted into company backends. Other changes:
    
    * the command can be now triggered manually using the standard company-complete command
    * automatic loading (with company-idle-delay) is no longer disabled
    * find-definition keybinding is left to users because the default M-. shouldn't be overridden
    * annotations appear immediately and are formatted a bit better
    * the completion triggers automatically after :: or . (customizable, defaults to true),
    otherwise it relies on company defaults (e.g. minimum prefix length)
    * TAB is left untouched

--- editors/racer.el
+++ editors/racer.el
@@ -22,6 +22,12 @@
   :type 'file
   :group 'racer)
 
+(defcustom racer-begin-after-member-access t
+  "When non-nil, begins completion automatically when the cursor is preceded
+by :: or ."
+  :type 'boolean
+  :group 'racer)
+
 (defvar racer-file-name)
 (defvar racer-tmp-file-name)
 (defvar racer-line-number)
@@ -46,119 +52,56 @@
     (setq racer-line-number (racer-get-line-number))
     (setq racer-column-number (current-column))
     (setq racer-completion-results `())
-    (write-region nil nil tmp-file-name))
+    (write-region nil nil tmp-file-name nil -1))
 
 (defun racer--candidates ()
   "Run the racer complete command and process the results."
   (setq racer-tmp-file-name (concat (buffer-file-name) ".racertmp"))
   (racer--write-tmp-file racer-tmp-file-name)
   (setenv "RUST_SRC_PATH" (expand-file-name racer-rust-src-path))
-    (let ((lines (process-lines racer-cmd
-				"complete"
-				(number-to-string racer-line-number)
-				(number-to-string racer-column-number)
-				racer-tmp-file-name)))
-      (delete-file racer-tmp-file-name)
-      (dolist (line lines)
-	(when (string-match "^MATCH \\([^,]+\\),\\([^,]+\\),\\([^,]+\\),\\([^,]+\\),\\([^,]+\\),\\(.+\\)$" line)
-	  (let ((completion (match-string 1 line))
-		(linenum (match-string 2 line))
-		(colnum (match-string 3 line))
-		(fname (match-string 4 line))
-		(matchtype (match-string 5 line))
-		(contextstr (match-string 6 line)))
-	    (put-text-property 0 1 'contextstr contextstr completion)
-	    (put-text-property 0 1 'matchtype matchtype completion)
-	    (push completion racer-completion-results))))
-      racer-completion-results))
-
-(defun racer--prefix ()
-  "Run the racer prefix command and process the results."
-  (setq racer-tmp-file-name (concat (buffer-file-name) ".racertmp"))
-  (racer--write-tmp-file racer-tmp-file-name)
-  (setenv "RUST_SRC_PATH" (expand-file-name racer-rust-src-path))
   (let ((lines (process-lines racer-cmd
-			      "prefix"
-			      (number-to-string racer-line-number)
-			      (number-to-string racer-column-number)
-			      racer-tmp-file-name)))
+                              "complete"
+                              (number-to-string racer-line-number)
+                              (number-to-string racer-column-number)
+                              racer-tmp-file-name)))
     (delete-file racer-tmp-file-name)
-    (when (string-match "^PREFIX \\(.+\\),\\(.+\\),\\(.*\\)$" (nth 0 lines))
-      (match-string 3 (nth 0 lines)))))
+    (dolist (line lines)
+      (when (string-match "^MATCH \\([^,]+\\),\\([^,]+\\),\\([^,]+\\),\\([^,]+\\),\\([^,]+\\),\\(.+\\)$" line)
+        (let ((completion (match-string 1 line))
+              (linenum (match-string 2 line))
+              (colnum (match-string 3 line))
+              (fname (match-string 4 line))
+              (matchtype (match-string 5 line))
+              (contextstr (match-string 6 line)))
+          (put-text-property 0 1 'contextstr contextstr completion)
+          (put-text-property 0 1 'matchtype matchtype completion)
+          (put-text-property 0 1 'fname fname completion)
+          (put-text-property 0 1 'linenum linenum completion)
+          (push completion racer-completion-results))))
+    racer-completion-results))
 
-(defun racer--complete-at-point-fn ()
-  "Run the racer complete command and process the results."
-  (setq racer-tmp-file-name (concat (buffer-file-name) ".racertmp"))
-  (racer--write-tmp-file racer-tmp-file-name)
-  (setenv "RUST_SRC_PATH" (expand-file-name racer-rust-src-path))
-  (save-excursion
-    (let ((lines (process-lines racer-cmd
-				"complete"
-				(number-to-string racer-line-number)
-				(number-to-string racer-column-number)
-				racer-tmp-file-name)))
-      (delete-file racer-tmp-file-name)
-      (dolist (line lines)
-	(when (string-match "^MATCH \\([^,]+\\),\\(.+\\)$" line)
-	  (let ((completion (match-string 1 line)))
-	    (push completion racer-completion-results)))
-	
-	(when (string-match "^PREFIX \\(.+\\),\\(.+\\),\\(.*\\)$" line)
-	  (setq racer-start-pos (string-to-number (match-string 1 line)))
-	  (setq racer-end-pos (string-to-number (match-string 2 line))))
-	)
-      )
-    )
-  ;(message "start %s end %s" start end)
-  (list (- (point) (- racer-end-pos racer-start-pos))
-        (point)
-        racer-completion-results)
-  )
+(defun racer--company-prefix ()
+  "Returns the symbol to complete. If racer-begin-after-member-access is t,
+begins completion automatically if the cursor is followed by a dot or ::."
+  (if racer-begin-after-member-access
+    (company-grab-symbol-cons "\\.\\|::" 2)
+    (company-grab-symbol)))
 
-(defun racer-company-complete (command &optional arg &rest ignored)
-  "Run the racer command for `COMMAND' and format using `ARG'.
-`IGNORED' is unused."
-  (interactive)
-  ;(message "PHIL racer-company-complete %s %s %s" command arg ignored)
-  (when (looking-back "[a-zA-z1-9:.]")
-    (cl-case command
-      (prefix (racer--prefix))
-      (candidates (racer--candidates))
-      (duplicates t)
-      (sorted nil)
-      (annotation
-       (progn
-	 (format "%s %10s : %s"
-		 (make-string (max 0 (- 20 (length arg))) ? )
-		 (get-text-property 0 'matchtype arg)
-		 (get-text-property 0 'contextstr arg))))
-      (meta (format "%s" (get-text-property 0 'contextstr arg)))
-;      (doc-buffer (company-doc-buffer "PHil hello Yeah"))
-      )))
+(defun racer--company-annotation (arg)
+  "Gets and formats annotation data from the arg match."
+  (let ((meta (get-text-property 0 'contextstr arg)))
+    (format "%10s : %s"
+            (or (save-match-data
+                  (and (string-match "\\(.+\\) {" meta)
+                       (match-string 1 meta)))
+                meta)
+            (get-text-property 0 'matchtype arg))))
 
-;; (defun racer-complete (command &optional arg &rest ignored)
-;;   (interactive)
-;;   (message "PHIL racer-company-complete %s %s %s" command arg ignored)
-;;   (when (looking-back "[a-zA-z1-9:.]")
-;;     (case command
-;;       (prefix (racer--prefix))
-;;       (candidates (racer--candidates))
-;;       (duplicates t)
-;;       (sorted nil)
-;;       (annotation (format ":%s yeah mother" arg))
-;;       (meta (format "This value is named %s" arg))
-;; ;      (doc-buffer (company-doc-buffer "PHil hello Yeah"))
-;;       )))
+(defun racer--company-location (arg)
+  (let ((fname (get-text-property 0 'fname arg))
+        (linenum (get-text-property 0 'linenum arg)))
+    (cons fname (string-to-number linenum))))
 
-
-;; (add-hook 'completion-at-point-functions 'racer-complete nil)
-
-(defun racer--complete-or-indent ()
-  "Complete with company-mode or indent."
-  (interactive)
-  (if (company-manual-begin)
-      (company-complete-common)
-    (indent-according-to-mode)))
 
 (defun string/ends-with (s ending)
   "Return non-nil if string S ends with ENDING."
@@ -166,6 +109,7 @@
          (let ((elength (length ending)))
            (string= (substring s (- 0 elength)) ending)))
         (t nil)))
+
 
 (defun racer-find-definition ()
   "Run the racer find-definition command and process the results."
@@ -193,15 +137,22 @@
 	  (forward-char (string-to-number charnum))
 	  )))))
 
-(add-hook 'rust-mode-hook
-	  '(lambda ()
-	     (company-mode)
-	     (set (make-local-variable 'company-backends) '(racer-company-complete))
-	     (local-set-key "\t" 'racer--complete-or-indent)
-	     (local-set-key "\M-." 'racer-find-definition)
-	     (setq-local company-idle-delay nil)
-	     )
-	  nil)
+;;;###autoload
+(defun racer-company-complete (command &optional arg &rest ignored)
+  "Run the racer command for `COMMAND' and format using `ARG'.
+`IGNORED' is unused."
+  (interactive)
+  (cl-case command
+    (interactive (company-begin-backend 'racer-company-complete))
+    (prefix (and (derived-mode-p 'rust-mode)
+                 (not (company-in-string-or-comment))
+                 (or (racer--company-prefix) 'stop)))
+    (candidates (racer--candidates))
+    (duplicates t)
+    (location (racer--company-location arg))
+    (sorted nil)
+    (meta (format "%s" (get-text-property 0 'contextstr arg)))
+    (annotation (racer--company-annotation arg))))
 
 (provide 'racer)
 ;;; racer.el ends here
