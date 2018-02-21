f74558e5c913ab6264bcf940b1f996d0eb0e31be
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-02-21 22:55:52 +0900

    made comments in test_project more comprehensive

--- tests/system.rs
+++ tests/system.rs
@@ -4190,9 +4190,25 @@ fn find_crate_doc() {
     extern crate fixtures;
     use fixtur~
     ";
+    let doc_str =
+r#"This is a test project for racer.
 
+# Example:
+Basic Usage.
+
+```
+extern crate test_fixtures;
+use test_fixtures::foo;
+fn main {
+    println!("Racer")
+}
+```
+
+## Notes:
+- We should check racer can parse rust doc style comments
+- and some comments..."#;
     within_test_project(|| {
         let got = get_one_completion(src, None);
-        assert_eq!("This is a test project for racer.", got.docs);
+        assert_eq!(doc_str, got.docs);
     })
 }
